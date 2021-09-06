40bfadde09c0c5218aa61707d02b4cd741672ad7
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-09-06 17:33:16 -0300

    bullet

---
+++ Spaceship/Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(0,speed);
+    }
+    public Bullet(float speed){
+        this.speed = speed;
+    }
+}
--- Spaceship/Assets/Scripts/Spaceship_player.cs
+++ Spaceship/Assets/Scripts/Spaceship_player.cs
@@ -7,6 +7,8 @@ public class Spaceship_player : MonoBehaviour
     public float horizontal;
     public float runSpeed = 10f;
     public Rigidbody2D rb;
+
+    public GameObject bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,15 +19,26 @@ public class Spaceship_player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        controlHorizontal();
+        fire();
+        
+        
+
+    }
+    public void controlHorizontal(){
         horizontal = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(horizontal*runSpeed,0);
         if(transform.position.x < 1.6f && transform.position.x > -1.6f){
-            
+            Debug.Log("in game");
             
         }else{
             Debug.Log("deny");
         }
-        
-
+    }
+    public void fire(){
+        if (Input.GetButtonDown("Space"))
+        {
+            Instantiate(bullet, transform.position);
+        }
     }
 }

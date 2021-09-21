a2fa83f1e726caf7922e04d88fe7f81becc65226
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-09-21 12:58:51 -0300

    bala do inimigo funcionando

--- Spaceship/Assets/Scripts/EnemyBullet.cs
+++ Spaceship/Assets/Scripts/EnemyBullet.cs
@@ -9,14 +9,13 @@ public class EnemyBullet : MonoBehaviour
     public Rigidbody2D rb;
     public Vector2 firstPosition;
     public GameObject player;
-    public float lifetime = 10f;
+    public float lifetime = 4f;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         player = GameObject.FindGameObjectWithTag("Player");
-        //speed = 1f;
-        firstPosition = transform.position;
+        goToTarget();
         
     }
     /// <summary>
@@ -30,20 +29,19 @@ public class EnemyBullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Destroy(gameObject,lifetime);
+    }
+
+    /// <summary>
+    /// Metodo para a bala rotacionar e ir em direcao ao player
+    /// </summary>
+    public void goToTarget(){
+        firstPosition = transform.position;
         Vector3 screenPoint = Camera.main.WorldToScreenPoint(firstPosition);
-        //Debug.Log(" screenpoint"+ screenPoint);
-        //rb.velocity = velocidade;
         Vector3 target = player.transform.position;
         Vector2 offset = new Vector2(target.x - firstPosition.x,target.y-firstPosition.y);
         float angulo = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f,0f,angulo);
-
         rb.velocity = transform.right * 10f;
-        
-    }
-    public void bullet_setTarget(){
-       //Vector2 vetorMovimento  = new Vector2(target.x - transform.position.x, target.y - transform.position.y);
-        //Debug.Log("velocidadevector:" + vetorMovimento+ " posicao target: "+ target);
-        //velocidade = vetorMovimento;
     }
 }

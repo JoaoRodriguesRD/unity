f3fdad219365f53130ff59f302ea71a118e08898
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-10-28 19:59:13 -0300

    Finalizando first spawner

--- Spaceship/Assets/Scripts/Spawner.cs
+++ Spaceship/Assets/Scripts/Spawner.cs
@@ -6,16 +6,21 @@ public class Spawner : MonoBehaviour
 {
     public GameObject prefab_enemy;
     public Vector2 SpawnPosition;
+    public int rate;
+    private int framecount=0;
     void Start()
     {
+        rate = 2000;
+        SpawnPosition = this.transform.position;
 
     }
 
     void Update()
     {
-        if(Time.frameCount % 2 == 0){
+        framecount++;
+        if(framecount > rate){
+            framecount = 0;
             Instantiate(prefab_enemy, SpawnPosition, this.transform.rotation);
         }
-        
     }
 }

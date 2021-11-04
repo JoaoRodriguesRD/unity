c2a4fa84cb571b1e278f31507c028ae9286eb0eb
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-11-04 20:26:24 -0300

    Script movimento em grid
    Script para andar em grid. Baseado em interação com os Tilemap background(permitido andar) e wall(Não permitido andar). Podendo ser usado de base para outras mecânicas

---
+++ scripts_auxiliares/GridMovement.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class GridMovement : MonoBehaviour
+{
+    public bool isMoving;
+    public float timeToMove = 0.2f;
+    public Vector3 origem,destino;
+
+    public Tilemap background;
+    public Tilemap wall;
+    void Start()
+    {
+        
+    }
+    void Awake(){
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W) && !isMoving){
+
+            MovePlayerLogic(Vector3.up);
+        }
+        if(Input.GetKey(KeyCode.A) && !isMoving){
+            MovePlayerLogic(Vector3.left);
+        }
+        if(Input.GetKey(KeyCode.S) && !isMoving){
+            MovePlayerLogic(Vector3.down);
+        }
+        if(Input.GetKey(KeyCode.D) && !isMoving){
+            MovePlayerLogic(Vector3.right);
+        }
+    }
+
+    private void MovePlayerLogic(Vector3 direction)
+    {
+        ;
+        if(canMove(direction)){
+            
+            StartCoroutine(Move(direction));
+        }else{
+            
+            DontMove(direction);
+        }
+        
+    }
+
+    private void DontMove(Vector3 direction)
+    {
+        Debug.Log("Não pode mover para: "+ direction);
+    }
+
+    private IEnumerator Move(Vector3 direction)
+    {
+        Debug.Log("Pode mover para+ " +direction);
+        isMoving = true;
+        float elapseTime = 0;
+        origem = transform.position;
+        destino = origem + direction;
+
+        while(elapseTime < timeToMove){
+            transform.position = Vector3.Lerp(origem,destino, elapseTime/timeToMove);
+            elapseTime += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = destino;
+        isMoving = false;
+    }
+
+    private bool canMove(Vector3 direction){
+        Vector3Int grigpos = background.WorldToCell(transform.position + direction);
+        if(wall.HasTile(grigpos)){
+            return false;
+        }
+        return true;
+    }
+}

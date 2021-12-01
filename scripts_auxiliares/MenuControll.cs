b780772a2eb92592224b26b395aee4e725e1b70b
João Rodrigues De Melo Neto <j_neto_rodrigues@hotmail.com>
2021-12-01 12:36:40 -0300

    Controle do menu
    Removendo redundancias...

--- scripts_auxiliares/MenuControll.cs
+++ scripts_auxiliares/MenuControll.cs
@@ -1,31 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuControll : MonoBehaviour
 {
     public int local = 0;
     int maxlocal;
-    public RectTransform rt;
+    public RectTransform rectTransform;
     public RectTransform[] AllNavigationsButtons;
-    private Vector2 uiOffset;
     Canvas canvas;
-
-    public static Vector2 pixelAjustpoint;
 
     void Start()
     {
-        rt = gameObject.GetComponent<RectTransform>();
-        canvas = gameObject.GetComponent<Canvas>();
+        rectTransform = gameObject.GetComponent<RectTransform>();
         maxlocal = AllNavigationsButtons.Length - 1;
         
     }
     void Update()
     {
-        print("max: " + maxlocal);
-        pixelAjustpoint = RectTransformUtility.PixelAdjustPoint(Vector2.up, rt, canvas);
-        print(pixelAjustpoint);
-
         if (Input.GetKeyDown(KeyCode.W))
         {
             local--;
@@ -45,17 +35,13 @@ public class MenuControll : MonoBehaviour
         }
 
         VerifyLocal();
-
-        // foreach (RectTransform i in AllNavigationsButtons){
-        //     print(i.anchoredPosition);
-        // }
         
     }
     void VerifyLocal()
     {
         try
         {
-             rt.position = AllNavigationsButtons[local].position + (Vector3.left * 90);
+             rectTransform.position = AllNavigationsButtons[local].position + (Vector3.left * 90);
         }
         catch (System.IndexOutOfRangeException)
         {
